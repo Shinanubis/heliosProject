22e7a3f19fe70543667c6873882e88e536746247
Shinanubis <eric_maginot@hotmail.fr>
2020-09-22 10:03:57 +0200

    fix minor error

--- heliosProject/urls.py
+++ heliosProject/urls.py
@@ -5,6 +5,7 @@ from django.contrib import admin
 from django.urls import path
 from django.views.decorators.csrf import csrf_exempt
 from graphene_django.views import GraphQLView
+from django.views.generic import TemplateView
 
 
 urlpatterns = [
