1663b4bfce4148cdcf0c46bd77b50c9aec8138bb
Shinanubis <eric_maginot@hotmail.fr>
2020-10-13 11:26:04 +0200

    first front render

--- frontend/src/views/Home.vue
+++ frontend/src/views/Home.vue
@@ -1,61 +1,26 @@
-<template xmlns:v-if="http://www.w3.org/1999/html">
-  <div class="home">
-    <img alt="Vue logo" src="../assets/logo.png">
-    <div>
-    <div v-for="article in articles" :key="article.id">
-        {{article.title}}<br>
-        {{article.author.username}}<br>
-        {{article.content}}<br>
-    </div>
-  </div>
+<template>
+  <div id="home">
+    <articles>
+      <banner />
+    </articles>
+
+    <about />
+
+    <subscribe />
+
+    <social />
   </div>
 </template>
 
 <script>
-import axios from 'axios'
+  export default {
+    name: 'Home',
 
-export default {
-  name: 'Home',
-  components: {
-  },
-  data(){
-    return {
-      articles: []
-    }
-  },
-  async mounted () {
-    try {
-      let result = await axios({
-        method: 'POST',
-        url: 'http://127.0.0.1:8000/graphql/',
-        data: {
-          query: `
-          {
-            articles{
-              id
-              title
-              author{
-                username
-              }
-              slug
-              content
-              comment{
-                author
-                content
-                createdAt
-              }
-              category{
-                name
-              }
-              createdAt
-            }
-          }	`
-        }
-      })
-      this.articles = result.data.data.articles
-    } catch (error) {
-      console.error(error)
-    }
+    components: {
+      About: () => import('@/components/home/About'),
+      Articles: () => import('@/components/home/Articles'),
+      Banner: () => import('@/components/home/Banner'),
+      Subscribe: () => import('@/components/home/Subscribe'),
+    },
   }
-}
 </script>
